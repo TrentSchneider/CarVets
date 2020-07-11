cb750e8487fb77e483c688b38554afc84388ec4c
Trent Schneider <trentonschneider@gmail.com>
2020-07-11 12:49:40 -0400

    reworked the ajax call to list locations on the index page, updated the readme, and commented out the hiding search field

--- JSscript.js
+++ JSscript.js
@@ -11,21 +11,44 @@ var lati, long;
 
 // ajax call for pulling google place info
 // leaving commented out until we implement it to avoid needless calls
-// $.ajax({
-//   url:
-//     "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" +
-//     lati +
-//     "," +
-//     long +
-//     "&radius=1500&type=car_repair&key=AIzaSyD1SPf-ueMO6m5itNj_GSTabGu60sa7SeE",
-
-//   method: "GET",
-// }).then(function (response) {
-//   var locName = response.name,
-//   locRating = response.rating,
-//   locURL = response.url;
-// });
-
+$.ajax({
+  url:
+    "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" +
+    lati +
+    "," +
+    long +
+    "&radius=8100&type=car_repair&key=AIzaSyD1SPf-ueMO6m5itNj_GSTabGu60sa7SeE&pagetoken=5",
+  method: "GET",
+}).then(function (response) {
+  var locName = response.results.name,
+    locLong = response.results.location.lng,
+    locLat = response.results.location.lat;
+  $.ajax({
+    url:
+      "https://maps.googleapis.com/maps/api/place/findplacefromtext/output?input=" +
+      locName +
+      "inputtype=textquery&point:" +
+      locLat +
+      "," +
+      locLong +
+      "&key=AIzaSyD1SPf-ueMO6m5itNj_GSTabGu60sa7SeE",
+    method: "GET",
+  }).then(function (response) {
+    for (let i = 0; i < response.candidates.length; i++) {
+      var liName = response[i].name,
+        liOpenHours = response.condidates[i].opening_hours.weekday_text,
+        liAddress = response.candidates[i].formatted_address;
+      var addLocation = $("li");
+      var liLocName = $("p").text(liName);
+      var liLocHours = $("p").text(liOpenHours);
+      var liLocAddress = $("p").text(liAddress);
+      addLocation.append(liLocName);
+      // addLocation.append(liLocAddress);
+      // addLocation.append(liLocHours);
+      $("#locationList").append(addLocation);
+    }
+  });
+});
 // function for loading the modal using foundation
 function loadModal() {
   var locModal = $("div");
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Description
 
-CarVets is an app for finding car repair shops near the user's current location. It will pull up a map that displays the user's current location along with the 5 closest car repair shops. The user will then be able to select one of these locations and have a modal display
+CarVets is an app for finding car repair shops near the user's current location. It will pull up a map that displays the user's current location along with the 5 closest car repair shops. The user will then be able to select one of these locations and have a modal display basic information about the location including the location's phone number, a link to get directions, the location's address. This site incorporates Google's javascript Map API and Place API.
 
 ## Installation
 
